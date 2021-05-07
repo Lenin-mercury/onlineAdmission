0c63fd9755c358204a4ced08aed6edf0a2675207
Lenin Karthick <leninece.mit@gmail.com>
2021-05-07 21:44:38 +0530

    small changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -103,3 +103,5 @@ const Home = () => {
 };
 
 export default Home;
+
+
---
+++ src/pages/PrintPage/PrintPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './print.scss'
+
+const PrintPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PrintPage;
